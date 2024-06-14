c6f169b818dca276b7df5a827c132c02c23884e3
Kasper Pelle Jensen <kaj007@edu.zealand.dk>
2024-06-14 12:48:26 +0200

    Færdig

--- Program.cs
+++ Program.cs
@@ -16,9 +16,6 @@
             Console.WriteLine(manager.AddBooking(new DateOnly(2024, 6, 19), new TimeOnly(13, 00), 1, c));
             Console.WriteLine(manager.AddBooking(new DateOnly(2024,6,16),new TimeOnly(8,00),1,b));
             Console.WriteLine(manager.AddBooking(new DateOnly(2024, 6, 16), new TimeOnly(11, 00), 1, b));
-            //manager.AddBooking(new DateOnly(2024,6,15),new TimeOnly(8,00),new TimeOnly(9, 00),1,a);
-            //manager.AddBooking(new DateOnly(2024, 6, 15), new TimeOnly(10,00), new TimeOnly(11, 00), 2, b);
-            //manager.AddBooking(new DateOnly(2024, 6, 19), new TimeOnly(13, 00), new TimeOnly(14, 00), 1, c);
 
 
             Console.WriteLine(manager.ShowBookings());
